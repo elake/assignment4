70dfda6e710d7e6a316c0b304710b6004b4c7e0e
Eldon Lake <elake@cs.ualberta.ca>
2013-03-25 23:35:37 -0600

    Commented and cleaned normal

--- zombie-v2/normal.py
+++ zombie-v2/normal.py
@@ -28,17 +28,27 @@ class Normal(MoveEnhanced):
         self.set_size(random.uniform(self.get_min_size(), self.get_max_size()))
 
     def get_author(self):
-        return "Your names go here"
+        return "Connor Peck and Eldon Lake"
 
     def n_wall(self):
         """
-        returns the coordinates of a fake zombie that keeps you away from walls
+        This function is used to create a "fake zombie" for the influence map.
+        It works by detecting which walls you are closest to, and then making
+        the fake zombie get closer to you as you approach walls. This helps
+        you to avoid getting trapped against a wall or backed into a corner.
+        Returns the coordinates of the fake zombie.
+
+        Legend:
+        zinf  - Lower values give the fake zombie a greater influence.
+        x_trv - How far along the x axis you have travelled. 0-1.
+        y_trv - How far along the y axis you have travelled. 0-1.
         """
         zinf = 15
         (my_x, my_y) = (self.get_xpos(), self.get_ypos())
         (x_min, y_min, x_max, y_max) = agentsim.gui.get_canvas_coords()
         x_trv = (my_x / (x_max - x_min))
         y_trv = (my_y / (y_max - y_min))
+
         if x_trv > 0.5:
             x_closest = x_max
         else:
@@ -48,33 +58,34 @@ class Normal(MoveEnhanced):
         else:
             y_closest = y_min
 
-        """
-        if abs(0.5 - x_trv) > abs(0.5 - y_trv):
-            ny = my_y
-            nx = sum([my_x, x_closest]) / 2
-        else:
-            ny = sum([my_y, y_closest]) / 2
-            nx = my_x
-        """
-
-        ny = my_y + (zinf / (-0.5 + y_trv))
-        nx = my_x + (zinf / (-0.5 + x_trv))
+        # Generate coordinates based on current position of self.
+        try: ny = my_y + (zinf / (-0.5 + y_trv))
+        except ZeroDivisionError:
+            ny = float("inf") # If you're in the center, fake zombie irrelevant.
+        try: nx = my_x + (zinf / (-0.5 + x_trv))
+        except ZeroDivisionError:
+            ny = float("inf")
 
         return (nx, ny)
 
     def compute_next_move(self):
-        # if we have a pending zombie alert, act on that first
+        # Pending zombie alerts are disregarded
         zombies = zombie.Zombie.get_all_present_instances()
         (dx, dy) = self.influence_map(zombies)
         
         x_pos = self.get_xpos()
         y_pos = self.get_ypos()
-        y_dir = (dy / abs(dy))
-        x_dir = (dx / abs(dx))
+        try: y_dir = (dy / abs(dy))
+        except ZeroDivisionError:
+            y_dir = 1 # Direction of 0 magnitude movement irrelevant.
+        try: x_dir = (dx / abs(dx))
+        except ZeroDivisionError:
+            x_dir = 1
         
          
         (x_min, y_min, x_max, y_max) = agentsim.gui.get_canvas_coords()
-    
+        
+        # This block changes running into walls into running along walls
         if not (x_min < x_pos + dx < x_max): # Out of bounds move.
             real_dx = min(x_min + x_pos, x_max - x_pos)
             dy = dy + (y_dir * (abs(dx)-abs(real_dx)))
@@ -84,6 +95,23 @@ class Normal(MoveEnhanced):
             dx = dx + (x_dir * (abs(dy)-abs(real_dy)))
             dy = y_dir*real_dy
         
+        """
+        The following lines prevent you from becoming trapped in a corner. Our
+        influene map currently uses "fake zombies" to prevent you from entering
+        a corner in the first place, but we left this because we feared someone
+        writing zombie code that somehow drove us into a corner despite our
+        influence map. This code comes into play when you've already entered a
+        corner. It determines if the average zombie position is above or below
+        the diagonal running out of the corner you're in, and then commits your
+        normal to crossing the corner at a 45 degree angle. Normals will not
+        change trajectory while crossing because that's how you get trapped.
+
+        Legend:
+        nf   - How close you need to be to the corner to cross it
+        gd   - Equals 1 if you are committed to crossing the corner
+        gr   - Equals 1 if you are committed in the opposite direction
+        """
+
         nf = 50
         
         try: gd
@@ -96,12 +124,12 @@ class Normal(MoveEnhanced):
             if self.avg_c_side(1, 1, zombies) or gd and not gr: # Go down
                 dx = self.get_move_limit() * -1
                 dy = self.get_move_limit()
-                gd = 1
+                gd = 1 # Commit to this direction
             else: # Go right
                 dx = self.get_move_limit()
                 dy = self.get_move_limit() * -1
                 gr = 1
-        else: (gr, gd) = (0, 0)
+        else: (gr, gd) = (0, 0) # You've finished crossing
 
         # Bottom left corner
         if self.get_xpos() < (x_min + nf) and self.get_ypos() > (y_max - nf):
@@ -159,47 +187,37 @@ class Normal(MoveEnhanced):
             (near_z, near_d) = nearest
             return near_z
 
-    def corner_side(self, z, d1, d2):
-        """
-        Determines which side a zombie is approaching a corner from. d1
-        determines whether the corner is on the top or bottom of the canvas
-        and d2 determines left or right.
-
-        Returns 0 for the Zombie approaching from the bottom and 1 for
-        the Zombie approaching from the top.
-        """
-        (zx, zy) = (z.get_xpos(), z.get_ypos())
-        (x_min, y_min, x_max, y_max) = agentsim.gui.get_canvas_coords()
-        x_travelled = (zx/ (x_max-x_min))
-        if d1*d2 < 0:
-            x_travelled = 1 - x_travelled
-        if x_travelled < (zy / (y_max-y_min)):
-                return 0
-        else: return 1
-
     def avg_c_side(self, d1, d2, zombies):
         """
         Determines which side the zombies are approaching a corner from. d1
-        determines whether the corner is on the top or bottom of the canvas
-        and d2 determines left or right.
+        determines whether the corner you are checking is on the top or bottom
+        of the canvas and d2 determines left or right. From there it checks if
+        the average zombie coordinate is above or below the diagonal drawn from
+        your corner.
 
         Returns 0 for the Zombie approaching from the bottom and 1 for
         the Zombie approaching from the top.
+
+        Legend:
+        
+        x_trv - Distance travelled across the x axis. 0-1.
         """
         (zx, zy) = center_of_mass(zombies)
         (x_min, y_min, x_max, y_max) = agentsim.gui.get_canvas_coords()
-        x_travelled = (zx/ (x_max-x_min))
+        x_trv = (zx/ (x_max-x_min))
         if d1*d2 < 0:
-            x_travelled = 1 - x_travelled
-        if x_travelled < (zy / (y_max-y_min)):
+            x_trv = 1 - x_trv
+        if x_trv < (zy / (y_max-y_min)):
                 return 0
         else: return 1
         
     def influence_map(self, zombies):
         ''' 
-        get a direction to go based on the influence map determined by zombies,
-        where each zombie influences my position based on the inverse of the
-        inverse of the magnitude of the vector of their position and mine.
+        Get a direction to go based on the influence map determined by all
+        other persons on the map. By accounting for all other persons and not
+        just zombies, we avoid getting stuck by colliding with one another. The
+        degree of influence that a person has on the map is inversely
+        proportional to their distance from self.
         '''
         (my_x, my_y) = (self.get_xpos(), self.get_ypos())
         
@@ -218,56 +236,20 @@ class Normal(MoveEnhanced):
         rel_vectors.append(Vector(my_x - self.n_wall()[0], 
                                   my_y - self.n_wall()[1]))
         
-        """
-        # don't get surrounded at the corner!
-        # currently uses incomplete functions. I apologize for how crappy this
-        # big block of if statements is. I'll hopefully have it cleaned up
-        # by the time I submit this.
-        of = 25
-        nf = 80
-        (min_x, min_y, max_x, max_y) = agentsim.gui.get_canvas_coords()
-        
-        # Top left corner
-        # if self.get_xpos() < (min_x + nf) and self.get_ypos() < (min_y + nf):
-        if self.corner_side(self.nearest_z(), 1, 1): # Go down
-            rel_vectors.append(Vector(my_x - (min_x), my_y - (min_y)))
-        else: # Go right
-            rel_vectors.append(Vector(my_x - (min_x), my_y - (min_y)))
-            print("Zombie coming from {n}".format(n=self.corner_side(self.nearest_z(), 1, 1)))
-
-        # Bottom left corner
-        #if self.get_xpos() < (min_x + nf) and self.get_ypos() > (max_y - nf):
-        if self.corner_side(self.nearest_z(), -1, 1): # Go up
-            rel_vectors.append(Vector(my_x - (min_x + 2*of), my_y - (max_y - of)))
-        else: # Go right
-            rel_vectors.append(Vector(my_x - (min_x + of), my_y - (max_y + of)))
-            print("Zombie coming from {n}".format(n=self.corner_side(self.nearest_z(), -1, 1)))
-
-        # Top right corner
-        #if self.get_xpos() > (max_x - nf) and self.get_ypos() < (min_y + nf):
-        if self.corner_side(self.nearest_z(), 1, -1): # Go down
-            rel_vectors.append(Vector(my_x - (max_x - of), my_y - (min_y - of)))
-        else: # Go left
-            rel_vectors.append(Vector(my_x - (max_x + of), my_y - (min_y + of)))
-            
-        # Bottom right corner
-        #if self.get_xpos() > (max_x - nf) and self.get_ypos() > (max_y - nf):
-        if self.corner_side(self.nearest_z(), -1, -1): # Go left
-            rel_vectors.append(Vector(my_x - (max_x + of), my_y - (max_y-of)))
-        else: # Go up
-            rel_vectors.append(Vector(my_x - (max_x - of), my_y - (max_y+of)))
-        """
         move_to = Vector(0, 0)
         for v in rel_vectors:
             v = v * (1 / v.magnitude() ** 3)
             move_to = move_to + v
         
         move_to = move_to.normalize() * self.get_move_limit()
-        print("self.get_move_limit() = {s}".format(s=self.get_move_limit()))
-        print("move to x: {a} move to y: {b}".format(a=move_to.x(), b=move_to.y()))
+
         return (move_to.x(), move_to.y())
 
     def zombie_alert(self, x_dest, y_dest):
+        """
+        This code was provided in class, and left here for completeness' sake,
+        but is never actually called and can be ignored.
+        """
         # ignore any request not from a defender!
         caller_name = callername.caller_name()
 
@@ -283,7 +265,7 @@ class Normal(MoveEnhanced):
 
 def center_of_mass(persons):
     '''
-    get and return the center of mass of the given persons
+    Get and return the center of mass of the given persons
     '''
     zxs = [z.get_xpos() for z in persons]
     zys = [z.get_ypos() for z in persons]
@@ -340,16 +322,22 @@ class Vector():
             new_x = self.x() * other
             new_y = self.y() * other
             return Vector(new_x, new_y)
-        except: # dunno what the error might be, other should be scalar neway
+        except: # dunno what the error might be, other should be scalar anyway
             return self # I guess pretend none of this ever happened?
 
     def magnitude(self):
+        """
+        A simple function to return the magnitude of a direct distance.
+        """
         retval = (self.x() ** 2 + self.y() ** 2)**(1/2)
         if not retval:
             return (0.0000000000000000000001)
         return retval
 
     def normalize(self):
+        """
+        A simple function to normalize a vector.
+        """
         if (self.magnitude() == 0):
             return (float("inf"), float("inf"))
         return (self * (1 / self.magnitude()))
